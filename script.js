66e79e45d145db615e731368d1f549f436c9a053
Жангир Уакасов <zangiruakasov@gmail.com>
2022-10-23 21:46:06 +0600

    new correct version

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+// const getArr = (num) => {
+//     let arr = num.toString().split('')
+//     return arr;
+// }
+
+// console.log(getArr(123456789));
+
+
+
+
 const getArr = (num) => {
-    let arr = num.toString().split('')
+    let arr = [];
+    let last = 0;
+
+    while (num > 0){
+        last = num % 10;
+        arr.unshift(last);
+        num = Math.floor(num / 10);
+    }
     return arr;
+
 }
 
-console.log(getArr(123456789));
+console.log(getArr(+prompt()));
+
